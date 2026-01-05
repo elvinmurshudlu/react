e9bed6ed84bc5ba4b8fd652eae6f3ce171b5540f
elvinmurshudlu-mina <elvin.murshudlu@mina.az>
2026-01-05 08:37:14 +0400

    Commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import App from "./App.tsx"
+import { RouterProvider } from "react-router"
+import { routes } from "@/Routing.tsx"
+import { ConfigProvider } from "antd"
+import az_AZ from "antd/locale/az_AZ"
+import "dayjs/locale/az"
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <ConfigProvider locale={az_AZ}>
+            <RouterProvider router={routes} />
+        </ConfigProvider>
     </StrictMode>,
 )
